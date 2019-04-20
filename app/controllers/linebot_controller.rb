d18d72386e70e22ccd68af3605ad62ab79888770
vanri <vanri0415@gmail.com>
2019-04-21 00:03:53 +0900

    change code linebot_controller

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -5,8 +5,9 @@ class LinebotController < ApplicationController
 
   def client
     @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["lINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["lINE_CHANNEL_TOKEN"]}
+      config.channel_secret = ENV["lINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["lINE_CHANNEL_TOKEN"]
+    }
   end
 
   def callback
@@ -19,9 +20,9 @@ class LinebotController < ApplicationController
 
     events = client.parse_events_from(body)
 
-    events.each_char { |event|
+    events.each { |event|
       case event
-      when Line::Bot::Event::Message::
+      when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
           message = {
